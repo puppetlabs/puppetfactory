056305f087c7dfdb96dc7c9637eb8257feb1065e
Josh Samuelson <js@puppetlabs.com>
2015-04-16 20:55:09 -0700

    test manifest for open source master

---
+++ pltraining-puppetfactory/tests/init.pp
@@ -0,0 +1,27 @@
+# Open Source Puppet Master
+class puppetfactory::params {
+  ca_certificate_path => '/opt/puppet/share/puppet-dashboard/certs/ca_cert.pem',
+  certificate_path => '/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.cert.pem',
+  private_key_path => '/opt/puppet/share/puppet-dashboard/certs/pe-internal-dashboard.private_key.pem',
+
+  classifier_url => "http://{::fqdn}:4433/classifier-api"
+
+  puppet => '/usr/bin/puppet',
+  rake => '/usr/bin/rake',
+
+  dash_path => '/opt/puppet/share/puppet-dashboard',
+
+  docroot => '/opt/puppetfactory',
+  logfile => '/var/log/puppetfactory',
+  cert_path => 'certs',
+  user => 'admin',
+  password => 'admin',
+  container_name => 'centosagent',
+
+  confdir => '/etc/puppet/',
+  usersuffix => 'puppetlabs.vm',
+  puppetcode => '/root/puppetcode',
+
+  pe => false,
+}
+
