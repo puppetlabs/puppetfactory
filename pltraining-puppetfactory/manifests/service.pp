0166fbf7c16333b1df98351f31ab8b715ad826ab
Josh Samuelson <js@puppetlabs.com>
2015-05-21 09:32:30 -0700

    Increment version

--- pltraining-puppetfactory/manifests/service.pp
+++ pltraining-puppetfactory/manifests/service.pp
@@ -3,8 +3,8 @@ class puppetfactory::service {
     path => '/var/staging/'
   }
 
-  staging::file { 'puppetfactory-0.1.4.gem':
-    source  => 'puppet:///modules/puppetfactory/puppetfactory-0.1.4.gem'
+  staging::file { 'puppetfactory-0.1.5.gem':
+    source  => 'puppet:///modules/puppetfactory/puppetfactory-0.1.5.gem'
   }
 
   package { 'puppetclassify':
@@ -16,8 +16,8 @@ class puppetfactory::service {
   package { 'puppetfactory':
     ensure   => present,
     provider => gem,
-    source   => "${staging::path}/puppetfactory/puppetfactory-0.1.4.gem",
-    require  => Staging::File['puppetfactory-0.1.4.gem'],
+    source   => "${staging::path}/puppetfactory/puppetfactory-0.1.5.gem",
+    require  => Staging::File['puppetfactory-0.1.5.gem'],
     before   => Service['puppetfactory'],
   }
 
