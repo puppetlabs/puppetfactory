d2b2198450443d925d26091904d63fe3279a995d
Josh Samuelson <js@puppetlabs.com>
2015-05-27 12:56:41 -0700

    Update wrapper for intro

--- pltraining-puppetfactory/manifests/intro.pp
+++ pltraining-puppetfactory/manifests/intro.pp
@@ -2,6 +2,8 @@ class puppetfactory::intro {
   # Classroom for Intro to puppet course
   class { 'puppetfactory':
     # Put students' puppetcode directories somewhere obvious
-    puppetcode => '/root/puppetcode',
+    puppetcode       => '/root/puppetcode',
+    map_environments => true,
+    container_name   => 'centosagent',
   }
 }
