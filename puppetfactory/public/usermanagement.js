3e2c48fd5d7f1a9a3c24f724ed26d37ed122f6b7
Josh Samuelson <js@puppetlabs.com>
2015-06-24 09:56:35 -0700

    Add username check to js

--- puppetfactory/public/usermanagement.js
+++ puppetfactory/public/usermanagement.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+  function validate(fld) {
+    var error = "";
+    var legalChars = /^[a-z][a-z0-9]{2,}$/; // Allow only letters for first character and then alphanumeric
+
+    if (fld.value == "") {
+      if (!legalChars.test(fld.value)) {
+        fld.style.background = 'Yellow';
+        error = "Only alphanumeric characters are allowed in usernames.\n";
+        alert(error);
+        return '';
+      }
+      // Return an empty string if username is invalid
+      return fld;
+    };
+  };
+
   // toggle hide the newuser dialog
   $('#showuser').click(function(){
     $(this).hide();
@@ -13,7 +29,7 @@ $(document).ready(function(){
     $('#newuser').hide();
   });
 
-   // save the new user
+  // save the new user
   $('#save').click(function(){
     var username  = $('#user').val();
     var password  = $('#password').val();
@@ -27,6 +43,9 @@ $(document).ready(function(){
     $('#user').removeClass("fail");
     $('#password').removeClass("fail");
     $('#password2').removeClass("fail");
+
+    // Validate the user input and replace with blank if invalid
+    username = validate(username);
 
     if(username == '') {
       $('#user').attr("value", "");
@@ -47,20 +66,20 @@ $(document).ready(function(){
       $('#newuser').addClass("processing");
       $('#newuser table').activity({width: 5.5, space: 6, length: 13});
 
-/*
-      $.get("/new/"+username, function(data) {
-        console.log(data);
-        var results = jQuery.parseJSON(data);
-        if(results.status == 'success') {
-          location.reload();
-        }
-        else {
-          alert('Could not create user: ' + results.message);
-          $('#newuser').removeClass("processing");
-          $('#newuser table').activity(false);
-        }
-      });
-*/
+      /*
+         $.get("/new/"+username, function(data) {
+         console.log(data);
+         var results = jQuery.parseJSON(data);
+         if(results.status == 'success') {
+         location.reload();
+         }
+         else {
+         alert('Could not create user: ' + results.message);
+         $('#newuser').removeClass("processing");
+         $('#newuser table').activity(false);
+         }
+         });
+         */
       $.post('/new', {username: username, password: password}, function(data) {
         console.log(data);
         var results = jQuery.parseJSON(data);
