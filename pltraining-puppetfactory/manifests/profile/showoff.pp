ed6c69b077659ae21aad8186039e7b78bb6ceb3e
Ben Ford <ben.ford@puppetlabs.com>
2016-01-26 17:06:03 -0800

    Use copied source files rather than a trusted key
    This exposes us to much less risk. It will require that the instructor
    runs a (yet to be created) rake task to upload the selected courseware
    to the targeted VM. This will work in the physical classroom, as well as
    with virtual classrooms.
    
    TRAINTECH-360 #time 1h

--- pltraining-puppetfactory/manifests/profile/showoff.pp
+++ pltraining-puppetfactory/manifests/profile/showoff.pp
@@ -1,70 +1,32 @@
 class puppetfactory::profile::showoff (
-  String $preso,
   Optional[String] $password,
-  Boolean $virtual = true,
 ) {
-  $courses = [
-    'appropriate_module_design',
-    'aws',
-    'fundamentals',
-    'infrastructure_design',
-    'intro',
-    'managing_puppet_code',
-    'practical_hiera_usage',
-    'puppet_4_parser',
-    'troubleshooting',
-    'windows_essentials',
-    'writing_your_first_module'
-  ]
-  unless $preso in $courses { fail("${preso} is not recognized as a virtual course we deliver.") }
+  require showoff
+  require puppetfactory::profile::pdf_stack
 
-  $repository = $virtual ? {
-    true  => 'git@github.com:puppetlabs/courseware-virtual.git',
-    false => 'git@github.com:puppetlabs/courseware.git',
-  }
+  $courseware_source = '/root/courseware'
 
-  $github_host_key = "AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
-
-  include showoff
-  include puppetfactory::profile::pdf_stack
-
-  file { "/home/${showoff::user}/.ssh/id_rsa":
-    ensure  => file,
-    owner   => $showoff::user,
-    group   => $showoff::group,
-    mode    => '0600',
-    source  => '/root/.ssh/github_rsa',
-    require => Class['showoff'],
-  }
-
-  sshkey { 'github key':
-    name         => 'github.com',
-    host_aliases => '192.30.252.129',
-    type         => 'ssh-rsa',
-    target       => "/home/${showoff::user}/.ssh/known_hosts",
-    key          => $github_host_key,
-  }
-
-  vcsrepo { "${showoff::root}/courseware":
-    ensure   => present,
-    provider => git,
-    user     => $showoff::user,
-    source   => $repository,
-    require  => Sshkey['github key'],
+  # We use this resource so that any time an instructor uploads new content,
+  # the PDF files will be rebuilt via the dependent exec statement
+  # This source path will be created via a courseware rake task.
+  file { "${showoff::root}/courseware":
+    ensure   => directory,
+    owner    => $showoff::user,
+    mode     => '0644',
+    source   => $courseware_source,
     notify   => Exec['build_pdfs'],
   }
 
   exec { 'build_pdfs':
     command     => "rake watermark target=_files/share password=${password}",
-    cwd         => "${showoff::root}/courseware/${preso}",
+    cwd         => "${showoff::root}/courseware/",
     path        => '/bin:/usr/bin:/usr/local/bin',
     environment => ['HOME=/root'],
-    require     => Class['puppetfactory::profile::pdf_stack', 'showoff'],
     refreshonly => true,
   }
 
   showoff::presentation { $preso:
-    path     => "${showoff::root}/courseware/${preso}",
+    path     => "${showoff::root}/courseware/",
     require  => Vcsrepo["${showoff::root}/courseware"],
   }
 
