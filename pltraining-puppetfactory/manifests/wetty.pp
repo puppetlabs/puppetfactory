762e6ba29b47a1a74e352adcb253cf83acc83e7a
Josh Samuelson <js@puppetlabs.com>
2016-04-25 15:08:46 -0700

    Use proper systemd

--- pltraining-puppetfactory/manifests/wetty.pp
+++ pltraining-puppetfactory/manifests/wetty.pp
@@ -13,9 +13,9 @@ class puppetfactory::wetty {
     require => Exec['npm -g install npm'],
   }
 
-  file { '/etc/init.d/wetty':
+  file { '/etc/systemd/system/wetty.service':
     ensure => 'present',
-    mode   => '0755',
+    mode   => '0644',
     source => 'puppet:///modules/puppetfactory/wetty.conf',
   }
   service { 'wetty':
