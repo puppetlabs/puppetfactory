86d705f0b68854e0650db668525b03d830f981fe
Josh Samuelson <js@puppetlabs.com>
2016-08-02 09:31:49 -0700

    Updating puppetfactory gem

--- pltraining-puppetfactory/manifests/service.pp
+++ pltraining-puppetfactory/manifests/service.pp
@@ -3,8 +3,8 @@ class puppetfactory::service {
     path => '/var/staging/'
   }
 
-  staging::file { 'puppetfactory-0.3.10.gem':
-    source  => 'puppet:///modules/puppetfactory/puppetfactory-0.3.10.gem'
+  staging::file { 'puppetfactory-0.3.11.gem':
+    source  => 'puppet:///modules/puppetfactory/puppetfactory-0.3.11.gem'
   }
 
   package { 'puppetclassify':
@@ -16,8 +16,8 @@ class puppetfactory::service {
   package { 'puppetfactory':
     ensure   => present,
     provider => gem,
-    source   => "${staging::path}/puppetfactory/puppetfactory-0.3.10.gem",
-    require  => Staging::File['puppetfactory-0.3.10.gem'],
+    source   => "${staging::path}/puppetfactory/puppetfactory-0.3.11.gem",
+    require  => Staging::File['puppetfactory-0.3.11.gem'],
     before   => Service['puppetfactory'],
   }
 
