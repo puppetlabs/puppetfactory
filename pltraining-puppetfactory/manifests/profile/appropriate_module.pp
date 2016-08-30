7e14214d3059f835f374c2cfca5b92509bb553f3
Ben Ford <ben.ford@puppetlabs.com>
2016-08-30 10:41:35 -0700

    Removed unused profile classes
    These should all be ported to the classroom module by now. Having these
    around is confusing for the instructors.
    
    Please review and validate, @samuelson

--- pltraining-puppetfactory/manifests/profile/appropriate_module.pp
+++
@@ -1,6 +0,0 @@
-class puppetfactory::profile::appropriate_module (
-  $session_id = $puppetfactory::params::session_id,
-) inherits puppetfactory::params {
-  # Classroom for Appropriate Module Design course
-  contain puppetfactory::profile::default
-}
--- pltraining-puppetfactory/manifests/profile/intro.pp
+++
@@ -1,6 +0,0 @@
-class puppetfactory::profile::intro (
-  $session_id = $puppetfactory::params::session_id,
-) inherits puppetfactory::params {
-  # Classroom for Intro to puppet course
-  contain puppetfactory::profile::default
-}
