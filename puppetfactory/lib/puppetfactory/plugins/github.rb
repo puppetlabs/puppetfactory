d74ed22a0767d9172290a9e509a896739bd2905b
Ben Ford <ben.ford@puppetlabs.com>
2016-09-09 08:50:26 -0700

    Add links to user's control repo
    TRAINTECH-324 #resolved #time 15m

--- puppetfactory/lib/puppetfactory/plugins/github.rb
+++ puppetfactory/lib/puppetfactory/plugins/github.rb
@@ -6,28 +6,30 @@ class Puppetfactory::Plugins::Github < Puppetfactory::Plugins
   def initialize(options)
     super(options)
 
-    @path     = options[:dashboard_path]     || '/etc/puppetfactory/dashboard'
-    @interval = options[:dashboard_interval] || 5 * 60  # test interval in seconds
+    @gitserver   = options[:gitserver]
+    @gituser     = options[:gituser]
+    @controlrepo = options[:controlrepo]
+    @repomodel   = options[:repomodel]
   end
 
-  def render
-    erb :dashboard
+  def create(username, password)
+    true
   end
 
-  def start!
-
+  def delete(username)
+    true
   end
 
-  def update
-
-  end
-
-  def available_tests
-    Dir.chdir(@path) { tests=`rake list`.split }
-  end
-
-  def test_data
-    JSON.parse(File.read("#{@path}/output/summary.json"))
+  def userinfo(username, extended = false)
+    if @repomodel == :single
+      controlrepo = "#{@gitserver}/#{@gituser}/#{@controlrepo.chomp('.git')}/tree/#{username}"
+    else
+      controlrepo = "#{@gitserver}/#{username}/#{@controlrepo.chomp('.git')}"
+    end
+    {
+      :username    => username,
+      :controlrepo => controlrepo,
+    }
   end
 
 end
