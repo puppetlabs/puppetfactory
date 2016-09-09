505d14368b6ccffc78d1c45dc90c825fe0916bad
Ben Ford <ben.ford@puppetlabs.com>
2016-09-09 09:52:16 -0700

    Create user branches in control repo
    This will create user branches in the control repo under certain
    conditions. Works when:
    
    - operating in single repository model
    - has valid Github token with the right permissions configured
        * This **might not be a good idea!**
    
    TRAINTECH-695 #resolved #time 30m

--- puppetfactory/lib/puppetfactory/plugins/github.rb
+++ puppetfactory/lib/puppetfactory/plugins/github.rb
@@ -1,4 +1,4 @@
-require 'json'
+require 'octokit'
 require 'puppetfactory'
 
 class Puppetfactory::Plugins::Github < Puppetfactory::Plugins
@@ -10,21 +10,58 @@ class Puppetfactory::Plugins::Github < Puppetfactory::Plugins
     @gituser     = options[:gituser]
     @controlrepo = options[:controlrepo]
     @repomodel   = options[:repomodel]
+
+    # chomp so we can support repo names with or without the .git
+    @controlrepo.chomp!('.git')
+
+    if options[:githubtoken]
+      @client = Octokit::Client.new(:access_token => options[:githubtoken])
+      @client.user.login
+    end
   end
 
   def create(username, password)
+    # can only do anything on our own repo, and only if we're authorized!
+    return true unless @client and @repomodel == :single
+
+    begin
+      # can only do anything on our own repo!
+      repo = "#{@gituser}/#{@controlrepo}"
+      sha  = @client.branches(repo).select { |branch| branch[:name] == 'production' }.first[:commit][:sha]
+      @client.create_ref(repo, "heads/#{username}", sha)
+      $logger.info "Created Github user branch for #{username}"
+
+    rescue => e
+      $logger.error "Error creating Github user branch for #{username}"
+      $logger.error e.message
+      return false
+    end
+
     true
   end
 
   def delete(username)
+    # can only do anything on our own repo, and only if we're authorized!
+    return true unless @client and @repomodel == :single
+
+    begin
+      @client.delete_branch("#{@gituser}/#{@controlrepo}", username)
+      $logger.info "Deleted Github user branch for #{username}"
+
+    rescue => e
+      $logger.error "Error deleting Github user branch for #{username}"
+      $logger.error e.message
+      return false
+    end
+
     true
   end
 
   def userinfo(username, extended = false)
     if @repomodel == :single
-      controlrepo = "#{@gitserver}/#{@gituser}/#{@controlrepo.chomp('.git')}/tree/#{username}"
+      controlrepo = "#{@gitserver}/#{@gituser}/#{@controlrepo}/tree/#{username}"
     else
-      controlrepo = "#{@gitserver}/#{username}/#{@controlrepo.chomp('.git')}"
+      controlrepo = "#{@gitserver}/#{username}/#{@controlrepo}"
     end
     {
       :username    => username,
