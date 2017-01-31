5b5dc67fe53c04e3af61713e542da38c74415ce0
Josh Samuelson <js@puppet.com>
2017-01-30 20:09:12 -0800

    Don't break SSH

--- pltraining-puppetfactory/manifests/init.pp
+++ pltraining-puppetfactory/manifests/init.pp
@@ -138,6 +138,7 @@ class puppetfactory (
     password_authentication        => $allow_root,
     permit_root_login              => $allow_root,
     password_authentication_groups => ['puppetfactory'],
+    use_pam                        => 'yes',
     host_keys                      => ['/etc/ssh/ssh_host_rsa_key','/etc/ssh/ssh_host_ecdsa_key', '/etc/ssh/ssh_host_ed25519_key']
   }
 
