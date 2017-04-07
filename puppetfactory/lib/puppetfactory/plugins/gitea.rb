9dfd7f504ec7e0868b96c1f25c4137205a6c8416
Greg Larkin <greg.larkin@puppet.com>
2017-04-07 16:38:27 -0700

    Replace hard-coded values for the control repository with the parameter from the config.yaml file

--- puppetfactory/lib/puppetfactory/plugins/gitea.rb
+++ puppetfactory/lib/puppetfactory/plugins/gitea.rb
@@ -8,16 +8,18 @@ class Puppetfactory::Plugins::Gitea < Puppetfactory::Plugins
     super(options)
 
     @suffix         = options[:usersuffix]
+    @controlrepo    = options[:controlrepo]
+    @reponame       = File.dirname(@controlrepo)
     @gitea_path     = options[:gitea_path] || '/home/git/go/bin/gitea'
     @admin_username = options[:gitea_admin_username] || 'root'
     @admin_password = options[:gitea_admin_password] || 'puppetlabs'
     @gitea_port     = options[:gitea_port] || '3000'
 
     begin
-      `curl -su "#{@admin_username}:#{@admin_password}" --data 'clone_addr=https://github.com/puppetlabs-education/classroom-control-vf.git&uid=1&repo_name=classroom-control-vf' http://localhost:#{@gitea_port}/api/v1/repos/migrate`
+      `curl -su "#{@admin_username}:#{@admin_password}" --data "clone_addr=https://github.com/puppetlabs-education/#{@controlrepo}&uid=1&repo_name=#{@reponame}" http://localhost:#{@gitea_port}/api/v1/repos/migrate`
       FileUtils::mkdir_p '/var/cache/puppetfactory/gitea'
       Dir.chdir('/var/cache/puppetfactory/gitea') do
-        `git clone --depth 1 http://#{@admin_username}:#{@admin_password}@localhost:#{@gitea_port}/#{@admin_username}/classroom-control-vf.git`
+        `git clone --depth 1 http://#{@admin_username}:#{@admin_password}@localhost:#{@gitea_port}/#{@admin_username}/#{@controlrepo}`
       end
     rescue => e
       $logger.error "Error migrating repository: #{e.message}"
@@ -31,7 +33,7 @@ class Puppetfactory::Plugins::Gitea < Puppetfactory::Plugins
   end
 
   def make_branch(username)
-    Dir.chdir('/var/cache/puppetfactory/gitea/classroom-control-vf') do
+    Dir.chdir("/var/cache/puppetfactory/gitea/#{@reponame}") do
       `git checkout -b #{username} && git push origin #{username}`
     end
   end
@@ -42,7 +44,7 @@ class Puppetfactory::Plugins::Gitea < Puppetfactory::Plugins
         raise "Password must be at least 8 characters"
       end
       `su git -c "cd && #{@gitea_path} admin create-user --name #{username} --password #{password} --email #{username}.#{@suffix}"`
-      add_collaborator(@admin_username, 'classroom-control-vf', username, 'write')
+      add_collaborator(@admin_username, @reponame, username, 'write')
       make_branch(username)
       $logger.info "Created Gitea user #{username}."
     rescue => e
@@ -56,9 +58,9 @@ class Puppetfactory::Plugins::Gitea < Puppetfactory::Plugins
   def delete(username)
     begin
       `curl -su "#{@admin_username}:#{@admin_password}" -X "DELETE" http://localhost:#{@gitea_port}/api/v1/admin/users/#{username}`
-      $logger.info "Removed GitLab user #{username}."
+      $logger.info "Removed Gitea user #{username}."
     rescue => e
-      $logger.error "Error removing GitLab user #{username}: #{e.message}"
+      $logger.error "Error removing Gitea user #{username}: #{e.message}"
       return false
     end
 
