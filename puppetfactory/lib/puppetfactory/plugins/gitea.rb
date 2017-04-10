279f0821b6f4e964909efd5281cc6bd204cd47c9
Greg Larkin <greg.larkin@puppet.com>
2017-04-10 13:51:58 -0700

    Fixed the repository cloning process, refactored a bit

--- puppetfactory/lib/puppetfactory/plugins/gitea.rb
+++ puppetfactory/lib/puppetfactory/plugins/gitea.rb
@@ -9,23 +9,27 @@ class Puppetfactory::Plugins::Gitea < Puppetfactory::Plugins
 
     @suffix         = options[:usersuffix]
     @controlrepo    = options[:controlrepo]
-    @reponame       = File.dirname(@controlrepo)
+    @reponame       = File.basename(@controlrepo, '.git')
     @gitea_path     = options[:gitea_path] || '/home/git/go/bin/gitea'
     @admin_username = options[:gitea_admin_username] || 'root'
     @admin_password = options[:gitea_admin_password] || 'puppetlabs'
     @gitea_port     = options[:gitea_port] || '3000'
 
-    begin
-      `curl -su "#{@admin_username}:#{@admin_password}" --data "clone_addr=https://github.com/puppetlabs-education/#{@controlrepo}&uid=1&repo_name=#{@reponame}" http://localhost:#{@gitea_port}/api/v1/repos/migrate`
-      FileUtils::mkdir_p '/var/cache/puppetfactory/gitea'
-      Dir.chdir('/var/cache/puppetfactory/gitea') do
-        `git clone --depth 1 http://#{@admin_username}:#{@admin_password}@localhost:#{@gitea_port}/#{@admin_username}/#{@controlrepo}`
-      end
-    rescue => e
-      $logger.error "Error migrating repository: #{e.message}"
-      return false
-    end
+    @cache_dir = '/var/cache/puppetfactory/gitea'
 
+    if (!File.directory?("#{@cache_dir}/#{@reponame}"))
+      $logger.info "Migrating repository #{@reponame}"
+      begin
+        `curl -su "#{@admin_username}:#{@admin_password}" --data "clone_addr=https://github.com/puppetlabs-education/#{@controlrepo}&uid=1&repo_name=#{@reponame}" http://localhost:#{@gitea_port}/api/v1/repos/migrate`
+        FileUtils::mkdir_p @cache_dir
+        Dir.chdir(@cache_dir) do
+          `git clone --depth 1 http://#{@admin_username}:#{@admin_password}@localhost:#{@gitea_port}/#{@admin_username}/#{@controlrepo}`
+        end
+      rescue => e
+        $logger.error "Error migrating repository: #{e.message}"
+        return false
+      end
+    end
   end
 
   def add_collaborator(owner, repo, username, permission)
@@ -33,7 +37,7 @@ class Puppetfactory::Plugins::Gitea < Puppetfactory::Plugins
   end
 
   def make_branch(username)
-    Dir.chdir("/var/cache/puppetfactory/gitea/#{@reponame}") do
+    Dir.chdir("#{@cache_dir}/#{@reponame}") do
       `git checkout -b #{username} && git push origin #{username}`
     end
   end
@@ -43,7 +47,7 @@ class Puppetfactory::Plugins::Gitea < Puppetfactory::Plugins
       if password.length < 8
         raise "Password must be at least 8 characters"
       end
-      `su git -c "cd && #{@gitea_path} admin create-user --name #{username} --password #{password} --email #{username}.#{@suffix}"`
+      `su git -c "cd && #{@gitea_path} admin create-user --name #{username} --password #{password} --email #{username}@#{@suffix}"`
       add_collaborator(@admin_username, @reponame, username, 'write')
       make_branch(username)
       $logger.info "Created Gitea user #{username}."
